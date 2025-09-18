3359f924f4dbc23f54d4c4e06e4f4025b2ab936d
Darshan Lamichhane <lamichhane3848@gmail.com>
2025-09-18 14:21:45 +0545

    feat: add MongoDB connection

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+import 'dotenv/config'
 import { configDotenv } from 'dotenv'
 import express from 'express'
+import { connectDB } from './config/database.js'
 
+connectDB()
 const app = express()
 configDotenv()
 
 app.get('/', (req, res) => {
   res.send('route path /')
 })
+
+app.use(express.json());
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)

d19440ca610e31c35d94424cd72d015ce46a5646
Darshan Lamichhane <lamichhane3848@gmail.com>
2025-09-18 14:38:46 +0545

    feat: implement token generation and authentication middleware

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import 'dotenv/config'
 import { configDotenv } from 'dotenv'
 import express from 'express'
 import { connectDB } from './config/database.js'
+import Token from './models/Token.js'
+import jwt from 'jsonwebtoken'
+import User from './models/User.js'
 
 connectDB()
 const app = express()
@@ -16,3 +19,38 @@ app.use(express.json());
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`)
 })
+
+function generateToken() {
+  return jwt.sign({ id: userId}, process.env.JWT_SECRET, { expiresIn: '1h' })
+}
+
+// Authentication Middleware
+const authenticateToken = async (req, res, next) => {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if(!token) return res.status(401).json({error: 'Access token required'})
+
+  try {
+    const blackListedToken = await Token.findOne({token})
+    if(blackListedToken) {
+      return res.status(403).json({error: 'Token is no longer available'})
+    }
+    // Token Verification
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const user = await User.findById(decoded.id).select('-password -verificationToken')
+    if(!user) {
+      return res.status(401).json({error: 'User belonging to this token no longer exists'})
+    }
+    req.user = user
+    next()
+  } catch (error) {
+    if (error.name === 'JsonWebTokenError') {
+      return res.status(403).json({ error: 'Invalid token' });
+    }
+    if (error.name === 'TokenExpiredError') {
+      return res.status(403).json({ error: 'Token expired' });
+    }
+    res.status(500).json({ error: 'Internal server error during authentication' });
+  }    
+}
